7604eba65adb0a236e817ad5e84e8aff77787b51
leeSJ96 <tmdwn3214@naver.com>
2021-03-30 19:46:33 +0900

    바텀네비게이션뷰

--- app/src/main/java/com/example/running/ui/frgment/RunFragment.kt
+++ app/src/main/java/com/example/running/ui/frgment/RunFragment.kt
@@ -1,13 +1,27 @@
 package com.example.running.ui.frgment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.running.R
 import com.example.running.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run){
 
     private val viewModel: MainViewModel by viewModels()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+
+        }
+    }
 }
--- app/src/main/java/com/example/running/ui/frgment/SetupFragment.kt
+++ app/src/main/java/com/example/running/ui/frgment/SetupFragment.kt
@@ -1,7 +1,21 @@
 package com.example.running.ui.frgment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.running.R
+import com.example.running.databinding.FragmentSettingsBinding
+import kotlinx.android.synthetic.main.fragment_setup.*
 
-class SetupFragment : Fragment(R.layout.fragment_setup){
+class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
+
 }
