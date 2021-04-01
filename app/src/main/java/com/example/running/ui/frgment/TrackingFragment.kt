6ea65a31912a8163b433cea176410a86f76bf04c
leeSJ96 <tmdwn3214@naver.com>
2021-04-01 12:53:39 +0900

    기록 초기화 다이얼로그

--- app/src/main/java/com/example/running/ui/frgment/TrackingFragment.kt
+++ app/src/main/java/com/example/running/ui/frgment/TrackingFragment.kt
@@ -2,14 +2,16 @@ package com.example.running.ui.frgment
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.running.R
 import com.example.running.di.other.Constants
 import com.example.running.di.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.running.di.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.running.di.other.Constants.ACTION_STOP_SERVICE
 import com.example.running.di.other.Constants.MAP_ZOOM
 import com.example.running.di.other.Constants.POLYLINE_COLOR
 import com.example.running.di.other.Constants.POLYLINE_WIDTH
@@ -22,6 +24,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.PolygonOptions
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -36,6 +39,18 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var map: GoogleMap? = null
 
     private var curTimeInMillis = 0L
+
+    private var menu: Menu? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -71,10 +86,56 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private fun toggleRun() {
         if(isTracking) {
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracking_menu,menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if (curTimeInMillis > 0L){
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miCanceelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingDialog(){
+        val dialog = MaterialAlertDialogBuilder(requireContext(),R.style.AlertDialogTheme)
+            .setTitle("삭제")
+            .setMessage("삭제를 누르면 데이터가 사라집니다")
+            .setIcon(R.drawable.ic_delete)
+            .setPositiveButton("Yes" ){_,_ ->
+                stopRun()
+            }
+            .setNegativeButton("취소"){ dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .create()
+        dialog.show()
+    }
+
+
+
+    private fun stopRun(){
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
     }
 
     private fun updateTracking(isTracking: Boolean) {
@@ -84,6 +145,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             btnFinishRun.visibility = View.VISIBLE
         } else {
             btnToggleRun.text = "Stop"
+            menu?.getItem(0)?.isVisible =true
             btnFinishRun.visibility = View.GONE
         }
     }
